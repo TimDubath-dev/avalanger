c59da6769e82176b5e45ad050a87ef562b6b9289
Tim Dubath <155271392+TimDubath-dev@users.noreply.github.com>
2026-02-24 09:22:37 +0100

    add lombok to project

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,5 +1,6 @@
 package ch.zhaw.avalanger.controller;
 
+import ch.zhaw.avalanger.model.Avalange;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -8,13 +9,18 @@ import java.util.Optional;
 @RequestMapping("/api/avalange")
 public class AvalangeController {
 
-    @GetMapping({"/{country}", ""})
+    @GetMapping({"/{country}"})
     public String getAllAvalange(@PathVariable Optional<String> country, @RequestParam(required = false) String state) {
         if (country.isPresent()) {
             return "No avalange found with country: " + country.get() + " and state: " + state;
         } else {
             return "No avalange found " + country.get() + " and state: " + state;
         }
+    }
+
+    @PostMapping("")
+    public String createAvalange(@RequestBody Avalange avalange) {
+        return "Avalange created: " + avalange.getCountry() + ", " + avalange.getState() + ", " + avalange.getDescription();
     }
 }
 
---
+++ src/main/java/ch/zhaw/avalanger/model/Avalange.java
@@ -0,0 +1,15 @@
+package ch.zhaw.avalanger.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Avalange {
+
+    private String country;
+    private String state;
+    private String description;
+
+
+}
