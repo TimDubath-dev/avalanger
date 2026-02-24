e99a7ba40f0625264af42d704e7f0bf642b92db0
Tim Dubath <155271392+TimDubath-dev@users.noreply.github.com>
2026-02-24 09:06:05 +0100

    add country and state

--- src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
+++ src/main/java/ch/zhaw/avalanger/controller/AvalangeController.java
@@ -1,16 +1,20 @@
 package ch.zhaw.avalanger.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/avalange")
 public class AvalangeController {
 
-    @GetMapping
-    public String getAllAvalange() {
-        return "No avalange found";
+    @GetMapping({"/{country}", ""})
+    public String getAllAvalange(@PathVariable Optional<String> country, @RequestParam(required = false) String state) {
+        if (country.isPresent()) {
+            return "No avalange found with country: " + country.get() + " and state: " + state;
+        } else {
+            return "No avalange found " + country.get() + " and state: " + state;
+        }
     }
 }
 
